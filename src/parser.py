5e015fad6776a098969809932af21486f0307e98
Samantha Aguinaldo <112607392+jhamped@users.noreply.github.com>
2025-03-04 13:34:26 +0800

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -2,13 +2,22 @@ from definitions import *
 import tkinter as tk
 
 # Parsing table based on the provided grammar
-def parse(console):
+def parse(console):    
+    current_token_index = 0
+    
+    def error_message(error):
+        line = rows[current_token_index]
+        column = col[current_token_index]
+        console.insert(tk.END, "Syntax Error: ", "error")
+        console.insert(tk.END, f"{error}")
+        console.insert(tk.END, f"\n           line {line}, col {column}\n", "ln_col")
+
+
     add_all_set()
     if not token:  # If token list is empty
-        console.insert(tk.END, "Syntax Error: No tokens to parse")
+        error_message("No tokens to parse")
         return
     stack = ["<program>"]  # Initialize stack with start symbol and end marker
-    current_token_index = 0
 
     def get_lookahead():
         if current_token_index >= len(token):  # Prevent index out of range
@@ -26,11 +35,11 @@ def parse(console):
         if len(token) > current_token_index:
             lookahead = get_lookahead()
         else:
-            console.insert(tk.END, "Syntax Error")
+            error_message("End of file")
         top = stack.pop()
         lookahead = get_lookahead()
         if lookahead is None:
-            console.insert(tk.END, f"Syntax Error: Unexpected end of input. No main function found")
+            error_message("Unexpected end of input. No main function found")
             return
         if top == lookahead:
             # Terminal matches lookahead, consume the token
@@ -44,13 +53,13 @@ def parse(console):
                 if rule != ["null"]:  # Push right-hand side of rule onto stack (in reverse)
                     stack.extend(reversed(rule))
             else:
-                console.insert(tk.END, f"Syntax error: No rule for {top} with lookahead {lookahead}")
+                error_message(f"No rule for {token} with lookahead {lookahead}")
                 break
         else:
-            console.insert(tk.END, f"Syntax error: Unexpected symbol {lookahead}")
+            error_message(f"Unexpected symbol {lookahead}")
             break
 
-    if stack or current_token_index < len(token):
-        console.insert(tk.END, "\nInput rejected: Syntax error.")
-    else:
-        console.insert(tk.END, "Input accepted: Syntactically correct.")
+    #if stack or current_token_index < len(token):
+        #error_message("Input rejected")
+    #else:
+     #   console.insert(tk.END, "Input accepted: Syntactically correct.")
